935cab1483f685a40b285af94b734b7f3886c07c
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2020-10-24 22:26:30 +0530

    React and web app

--- cypress/integration/index_specs.js
+++ cypress/integration/index_specs.js
@@ -1,12 +1,6 @@
-const { expect } = require("chai");
-const { describe } = require("mocha");
-
 describe('Testing Simple App', () =>{
-
-    beforeEach(() => {
-        cy.visit('../index.html');
-    })
     it('Adding two +ve number', () =>{
+        cy.visit('./index.html');
         cy.get('#num1').type('1');
         cy.get('#num2').type('2');
         cy.get('#mybtn').click();
@@ -14,8 +8,12 @@ describe('Testing Simple App', () =>{
             var x = $result.val();
             expect(x).to.equal('3');
         });
-    });
+    })
+})
+
+describe('Testing Simple App', () =>{
     it('Adding +ve and -ve number', () =>{
+        cy.visit('./index.html');
         cy.get('#num1').type('3');
         cy.get('#num2').type('-4');
         cy.get('#mybtn').click();
@@ -24,32 +22,51 @@ describe('Testing Simple App', () =>{
             expect(x).to.equal('-1');
         });
     });
-    it('Addition with Zero', () =>{
-        cy.get('#num1').type('0');
-        cy.get('#num2').type('-1');
-        cy.get('#mybtn').click();
-        cy.get('#result').should(($result) =>{
-            var x = $result.val();
-            expect(x).to.equal('-1');
-        });
-    });
-    it('Adding two floating point numbers', () =>{
-        cy.get('#num1').type('2.5');
-        cy.get('#num2').type('3.2');
-        cy.get('#mybtn').click();
-        cy.get('#result').should(($result) =>{
-            var x = $result.val();
-            expect(x).to.equal('5.7');
-        });
-    })
-    it('Input other than number', () =>{
-        cy.get('#num1').type('qwe');
-        cy.get('#num1').should(($num1) =>{
-            expect($num1.val()).to.equal('');
-        })
-        cy.get('#num2').type('qwe');
-        cy.get('#num2').should(($num1) =>{
-            expect($num1.val()).to.equal('');
-        })
-    });
 })
+
+describe('Testing Simple App', () =>{
+    it('Addition with Zero', () =>{
+           cy.visit('./index.html');
+           cy.get('#num1').type('0');
+           cy.get('#num2').type('-1');
+           cy.get('#mybtn').click();
+           cy.get('#result').should(($result) =>{
+               var x = $result.val();
+               expect(x).to.equal('-1');
+           });
+       });
+   })
+
+   describe('Testing Simple App', () =>{
+    it('Adding two floating point numbers', () =>{
+            cy.visit('./index.html');
+            cy.get('#num1').type('2.5');
+            cy.get('#num2').type('3.2');
+            cy.get('#mybtn').click();
+            cy.get('#result').should(($result) =>{
+                var x = $result.val();
+                expect(x).to.equal('5.7');
+            });
+        })
+    })
+
+    describe('Testing Simple App', () =>{
+        it('Input other than number', () =>{
+                cy.visit('./index.html');
+                cy.get('#num1').type('qwe');
+                cy.get('#num1').should(($num1) =>{
+                    expect($num1.val()).to.equal('');
+                })
+                cy.get('#num2').type('qwe');
+                cy.get('#num2').should(($num1) =>{
+                    expect($num1.val()).to.equal('');
+                })
+            });
+        })
+
+        describe('Testing Simple App', () =>{
+            it('Result Inputbox Disabled', () =>{
+                cy.get('#result').should('be.disabled');
+            })
+        })
+        
